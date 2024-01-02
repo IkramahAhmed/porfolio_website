cb3728a280a464d29d6287b8aafc2acbf481ad4e
IkramahAhmed <ikramahahmed177@gmail.com>
2024-01-02 14:21:20 +0500

    Deploy the website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from '../src/components/header/Header';
+import Home from '../src/components/home/Home';
+import About from '../src/components/about/About';
+import Skills from '../src/components/skills/Skills';
+// import Services from '../src/components/services/Services';
+import Qualification from '../src/components/qualification/Qualification';
+import Work from '../src/components/work/Work';
+import Contact from './components/contact/Contactt';
+import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
+import EmailHome from './components/email-home/EmailHome';
+import SocialHome from './components/social-home/SocialHome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <>
+     <Header />
+     <main className='main'>
+
+      <Home />
+      <SocialHome />
+      <EmailHome />
+      <About />
+      <Skills />
+      {/* <Services /> */}
+      <Qualification />
+      <Work />
+      <Contact />
+     </main>
+
+      <Footer />
+     <ScrollUp />
+     </>
     </div>
   );
 }
